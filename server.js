06d02df12d893a8e15ed8e16e90b66aeab8af1ea
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-17 13:59:56 -0700

    Enabled cors on server side

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const pool = new Pool({
     user: 'bobby',
@@ -15,6 +15,7 @@ const pool = new Pool({
 app.get('/api/users', async (req,res) => {
     try {
         const data = await pool.query('SELECT * FROM users;')
+        res.set('Access-Control-Allow-Origin', '*')
         res.send(data.rows);   
     } catch (err) {
         console.error(err.message);
@@ -24,6 +25,7 @@ app.get('/api/users', async (req,res) => {
 app.get('/api/workout_plans', async (req,res) => {
     try {
         const data = await pool.query('SELECT * FROM workout_plans;')
+        res.set('Access-Control-Allow-Origin', '*')
         res.send(data.rows);
     } catch (err) {
         console.error(err.message)
@@ -33,10 +35,15 @@ app.get('/api/workout_plans', async (req,res) => {
 app.get('/api/workout', async (req,res) => {
     try {
         const data = await pool.query('SELECT * FROM workout;')
+        res.set('Access-Control-Allow-Origin', '*')
         res.send(data.rows)
     } catch (err) {
         console.error(err.message)
     }
+})
+
+app.use((req,res,next) => {
+    res.status(500).send("Internal Server Error")
 })
 
 app.listen(PORT, () =>{
