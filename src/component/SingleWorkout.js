32d29d1e13887e8fbe6b93aeed92b42c0e526fc1
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-17 16:34:41 -0700

    Created single workout component

---
+++ src/component/SingleWorkout.js
@@ -0,0 +1,9 @@
+const SingleWorkout = (props) => {
+    return (
+        <div className="singleWorkout">
+            <h2 className="singleWorkoutInfo">{props.exercise_name}</h2>
+        </div>
+    )
+}
+
+export default SingleWorkout
