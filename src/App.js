ad54561b3510f5c1c8577849f21331198d88bed6
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-17 14:28:30 -0700

    Added styling and hooks to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import GordonMedia from './img/Gordon Media.png';
 
 function App() {
+  const [users, setUsers] = useState(null);
+  const [plans, setPlan] = useState(null);
+  const [workout, setWorkout] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/users')
+    .then(response => response.json())
+    .then(data => setUsers({data}))
+  });
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/workout_plans')
+    .then(response => response.json())
+    .then(data => setPlan({data}))
+  })
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/workout')
+    .then(response => response.json())
+    .then(data => setWorkout({data}))
+  })
+
   return (
-    <div className="App">
-      
-    </div>
+      <>
+      <Header />
+        <div className="header">
+          <h1 className="title">Gordon Gym Trainer!</h1>
+          <img src={GordonMedia} alt="none" className='icon' />
+        </div>
+      </>
   );
 }
 
