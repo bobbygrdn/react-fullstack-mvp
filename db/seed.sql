c243a5e5c8ef3d49cfaec9413ffdfa7db6ee2e72
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-17 12:55:00 -0700

    Seeded data into two new tables

--- db/seed.sql
+++ db/seed.sql
@@ -1,13 +1,13 @@
 -- INSERT INTO users (USERNAME, HEIGHT, WEIGHT, BODY_FAT, BMI) VALUES ('bobbygrdn', 71, 185, 18, 22);
 -- INSERT INTO users (USERNAME, HEIGHT, WEIGHT, BODY_FAT, BMI) VALUES ('kcarter', 62, 190, 20, 24);
 
-INSERT INTO workout_plans (plan_name, type_of_plan, length_of_plan, user_id) VALUES ('Summer Bod', 'Strength', '5 days per week', 1);
-INSERT INTO workout_plans (plan_name, type_of_plan, length_of_plan, user_id) VALUES ('Fat Shred', 'Cutting', '5 days per week', 2);
+-- INSERT INTO workout_plans (plan_name, type_of_plan, length_of_plan, user_id) VALUES ('Summer Bod', 'Strength', '5 days per week', 1);
+-- INSERT INTO workout_plans (plan_name, type_of_plan, length_of_plan, user_id) VALUES ('Fat Shred', 'Cutting', '5 days per week', 2);
 
-INSERT INTO workout (exercise_name, sets, reps_time, rest_cycle, plan_id) VALUES ('Barbell Bench Press', '5', '5', 120 'Seconds', 1);
-INSERT INTO workout (exercise_name, sets, reps_time, rest_cycle, plan_id) VALUES ('Barbell Deadlift', '5', '5', 120 'Seconds', 1);
-INSERT INTO workout (exercise_name, sets, reps_time, rest_cycle, plan_id) VALUES ('Barbell Shoulder Press', '5', '5', 120 'Seconds', 1);
+-- INSERT INTO workout (exercise_name, sets, reps_time, rest_cycle, plan_id) VALUES ('Barbell Bench Press', '5', '5', '120 Seconds', 1);
+-- INSERT INTO workout (exercise_name, sets, reps_time, rest_cycle, plan_id) VALUES ('Barbell Deadlift', '5', '5', '120 Seconds', 1);
+-- INSERT INTO workout (exercise_name, sets, reps_time, rest_cycle, plan_id) VALUES ('Barbell Shoulder Press', '5', '5', '120 Seconds', 1);
 
-INSERT INTO workout (exercise_name, sets, reps_time, rest_cycle, plan_id) VALUES ('Push ups', '3', '15', 60 'Seconds', 2);
-INSERT INTO workout (exercise_name, sets, reps_time, rest_cycle, plan_id) VALUES ('Sit ups', '3', '15', 60 'Seconds', 2);
-INSERT INTO workout (exercise_name, sets, reps_time, rest_cycle, plan_id) VALUES ('Leg Raises', '3', '15', 60 'Seconds', 2);
+-- INSERT INTO workout (exercise_name, sets, reps_time, rest_cycle, plan_id) VALUES ('Push ups', '3', '15', '60 Seconds', 2);
+-- INSERT INTO workout (exercise_name, sets, reps_time, rest_cycle, plan_id) VALUES ('Sit ups', '3', '15', '60 Seconds', 2);
+-- INSERT INTO workout (exercise_name, sets, reps_time, rest_cycle, plan_id) VALUES ('Leg Raises', '3', '15', '60 Seconds', 2);
