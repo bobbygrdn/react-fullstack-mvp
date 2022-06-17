1ec40fa6ddb8f2b5a4f199e8b5d25894bcf94ccd
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-17 12:53:05 -0700

    Migrated two new tables

--- db/migration.sql
+++ db/migration.sql
@@ -1,7 +1,7 @@
 -- DROP TABLE IF EXISTS users;
 -- DELETE IF EXISTS calendar;
-DELETE IF EXISTS workout_plans;
-DELETE IF EXISTS workout;
+-- DELETE IF EXISTS workout_plans;
+-- DELETE IF EXISTS workout;
 -- DELETE IF EXISTS exercise_list;
 -- DELETE IF EXISTS exercises;
 
@@ -14,19 +14,19 @@ DELETE IF EXISTS workout;
 --     bmi INT NOT NULL
 -- );
 
-CREATE TABLE workout_plans {
-    plan_id SERIAL PRIMARY KEY,
-    plan_name VARCHAR(20) NOT NULL,
-    type_of_plan VARCHAR(20) NOT NULL,
-    length_of_plan VARCHAR(20) NOT NULL,
-    user_id INT FOREIGN KEY NOT NULL
-};
+-- CREATE TABLE workout_plans (
+--     plan_id SERIAL PRIMARY KEY,
+--     plan_name VARCHAR(20) NOT NULL,
+--     type_of_plan VARCHAR(20) NOT NULL,
+--     length_of_plan VARCHAR(20) NOT NULL,
+--     user_id INTEGER REFERENCES users(user_id)
+-- );
 
-CREATE TABLE workout {
-    workout_id SERIAL PRIMARY KEY,
-    exercise_name VARCHAR(30) NOT NULL,
-    sets INT NOT NULL,
-    reps_time TEXT NOT NULL,
-    rest_cycle TEXT NOT NULL,
-    plan_id INT FOREIGN KEY NOT NULL
-};
+-- CREATE TABLE workout (
+--     workout_id SERIAL PRIMARY KEY,
+--     exercise_name VARCHAR(30) NOT NULL,
+--     sets INT NOT NULL,
+--     reps_time TEXT NOT NULL,
+--     rest_cycle TEXT NOT NULL,
+--     plan_id INTEGER REFERENCES workout_plans(plan_id)
+);
