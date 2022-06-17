f814e85dde7c49f6890a089eccb2bebe95dc15b8
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-17 12:38:33 -0700

    Built server with basic get routes

--- server.js
+++ server.js
@@ -1,17 +1,44 @@
-require("dotenv").config();
 const express = require('express');
+const { Pool } = require('pg');
 const app = express();
-const db = require('./db/conn')
+
+const PORT = process.env.PORT || 4000;
+
+const pool = new Pool({
+    user: 'bobby',
+    host: 'localhost',
+    database: 'react_mvp_project',
+    password: '',
+    port: 5432,
+})
 
 app.get('/api/users', async (req,res) => {
     try {
-        const data = await db.query('SELECT * FROM users;')
+        const data = await pool.query('SELECT * FROM users;')
         res.send(data.rows);   
     } catch (err) {
         console.error(err.message);
     }
 });
 
-app.listen(process.env.PORT, () =>{
-    console.log(`Server is running on port ${process.env.PORT}`)
+app.get('api/workout_plans', async (req,res) => {
+    try {
+        const data = await pool.query('SELECT * FROM workout_plans;')
+        res.send(data.rows);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+app.get('api/workouts', async (req,res) => {
+    try {
+        const data = await pool.query('SELECT * FROM workout')
+        res.send(data.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
 })
