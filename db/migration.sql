702debc6816a4b06d80bbaf4f14582f817a7e41d
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-17 12:49:08 -0700

    Created workout plans table and workout table

--- db/migration.sql
+++ db/migration.sql
@@ -1,16 +1,32 @@
-DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS users;
 -- DELETE IF EXISTS calendar;
--- DELETE IF EXISTS workout_plans;
--- DELETE IF EXISTS workout;
+DELETE IF EXISTS workout_plans;
+DELETE IF EXISTS workout;
 -- DELETE IF EXISTS exercise_list;
 -- DELETE IF EXISTS exercises;
 
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    username VARCHAR(20) NOT NULL,
-    height INT NOT NULL,
-    weight INT NOT NULL,
-    body_fat INT NOT NULL,
-    bmi INT NOT NULL
-);
+-- CREATE TABLE users (
+--     user_id SERIAL PRIMARY KEY,
+--     username VARCHAR(20) NOT NULL,
+--     height INT NOT NULL,
+--     weight INT NOT NULL,
+--     body_fat INT NOT NULL,
+--     bmi INT NOT NULL
+-- );
 
+CREATE TABLE workout_plans {
+    plan_id SERIAL PRIMARY KEY,
+    plan_name VARCHAR(20) NOT NULL,
+    type_of_plan VARCHAR(20) NOT NULL,
+    length_of_plan VARCHAR(20) NOT NULL,
+    user_id INT FOREIGN KEY NOT NULL
+};
+
+CREATE TABLE workout {
+    workout_id SERIAL PRIMARY KEY,
+    exercise_name VARCHAR(30) NOT NULL,
+    sets INT NOT NULL,
+    reps_time TEXT NOT NULL,
+    rest_cycle TEXT NOT NULL,
+    plan_id INT FOREIGN KEY NOT NULL
+};
