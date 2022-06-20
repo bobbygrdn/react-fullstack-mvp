f29823127ba7159eb670566e3fd6702989f652b7
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-20 16:37:11 -0700

    Added more flexibility in component

--- src/component/UserProfile.js
+++ src/component/UserProfile.js
@@ -1,10 +1,12 @@
 const UserProfile = (props) => {
     return(
     <div className="profile">
-        <h1 className="userInfo">User Info: </h1>
-        <h2 className="userData">{props.users.data[0].username}</h2>
-        <h1 className="userInfo">User Statistics: </h1>
-        <h2 className="userData">Height: {props.users.data[0].height} Weight: {props.users.data[0].weight} Body Fat %: {props.users.data[0].body_fat} BMI: {props.users.data[0].bmi}</h2>
+        <h1 className="userData">{props.users.data[0].username}</h1>
+        <h1 className="userInfo">Statistics: </h1>
+        <h2 className="userStats">Height: {props.users.data[0].height}</h2>
+        <h2 className="userStats"> Weight: {props.users.data[0].weight}</h2>
+        <h2 className="userStats"> Body Fat %: {props.users.data[0].body_fat} </h2>
+        <h2 className="userStats"> BMI: {props.users.data[0].bmi}</h2>
     </div>
     )
 }
