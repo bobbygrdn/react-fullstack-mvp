dd3ea76a05c6caaf55fcf6da93f7b03100c6605b
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-20 15:43:44 -0700

    Finished workouts components with fetching data from server

--- src/component/SingleWorkout.js
+++ src/component/SingleWorkout.js
@@ -1,7 +1,8 @@
 const SingleWorkout = (props) => {
+
     return (
         <div className="singleWorkout">
-            <h2 className="singleWorkoutInfo">{props.exercise_name}</h2>
+            <h2 className="singleWorkoutInfo">{props.elem.exercise_name}: {props.elem.sets} X {props.elem.reps_time} Rest Cycle: {props.elem.rest_cycle}</h2>
         </div>
     )
 }
--- src/component/Workout.js
+++ src/component/Workout.js
@@ -1,9 +1,15 @@
 import SingleWorkout from './SingleWorkout'
 const Workouts = (props) => {
-    return (
-      props.workouts.data.map((elem) => {
-        return <SingleWorkout elem={elem} key={elem.workout_id} /> 
-        })
+
+  return (
+      <div className='workouts'>
+        <h1 className='workoutsInfo'>Exercises: </h1>
+          {props.workouts.data.map((elem) => {
+            return (        
+            <SingleWorkout elem={elem} key={elem.workout_id} />
+            )
+          })}
+      </div>
     )
 }
 
