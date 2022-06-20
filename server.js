ad88ac182402fa92f2fde4c6ee13239a17db82df
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-20 14:47:23 -0700

    Updated server to handle CORS at all levels

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.post('/api/users', async (req,res) => {
     const obj = req.body;
     try {
         const data = await pool.query(`INSERT INTO users (username, height, weight, body_fat, bmi) VALUES ('${obj.username}','${obj.height}', '${obj.weight}', '${obj.body_fat}', '${obj.bmi}');`)
+        res.set('Access-Control-Allow-Origin', '*')
         res.send('Created User')
     } catch (err) {
         console.error(err.message)
@@ -52,6 +53,7 @@ app.patch('/api/users/:id', async (req,res) => {
     if(obj.username) {
         try {
             const data = await pool.query(`UPDATE users SET username = '${obj.username}' WHERE user_id = '${id}';`)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send('Updated username');
         } catch (err) {
             console.error(err.message)
@@ -60,6 +62,7 @@ app.patch('/api/users/:id', async (req,res) => {
     if(obj.height) {
         try {
             const data = await pool.query(`UPDATE users SET height = '${obj.height}' WHERE user_id = '${id}';`)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send('Updated height');
         } catch (err) {
             console.error(err.message)
@@ -68,6 +71,7 @@ app.patch('/api/users/:id', async (req,res) => {
     if(obj.weight) {
         try {
             const data = await pool.query(`UPDATE users SET weight = '${obj.weight}' WHERE user_id = '${id}';`)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send('Updated weight');
         } catch (err) {
             console.error(err.message)
@@ -76,6 +80,7 @@ app.patch('/api/users/:id', async (req,res) => {
     if(obj.body_fat) {
         try {
             const data = await pool.query(`UPDATE users SET body_fat = '${obj.body_fat}' WHERE user_id = '${id}';`)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send('Updated body_fat');
         } catch (err) {
             console.error(err.message)
@@ -84,6 +89,7 @@ app.patch('/api/users/:id', async (req,res) => {
     if(obj.bmi) {
         try {
             const data = await pool.query(`UPDATE users SET bmi = '${obj.bmi}' WHERE user_id = '${id}';`)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send('Updated bmi');
         } catch (err) {
             console.error(err.message)
@@ -95,6 +101,7 @@ app.delete('/api/users/:id', async (req,res) => {
     const id = req.params.id;
     try {
         const data = await pool.query(`DELETE FROM users WHERE user_id = '${id}';`)
+        res.set('Access-Control-Allow-Origin', '*')
         res.send('Deleted User')
     } catch (err) {
         console.error(err.message)
@@ -126,6 +133,7 @@ app.post('/api/workout_plans', async (req,res) => {
     const obj = req.body;
     try {
         const data = await pool.query(`INSERT INTO workout_plans (plan_name, type_of_plan, length_of_plan) VALUES ('${obj.plan_name}','${obj.type_of_plan}', '${obj.length_of_plan}');`)
+        res.set('Access-Control-Allow-Origin', '*')
         res.send('Created Workout Plan')
     } catch (err) {
         console.error(err.message)
@@ -139,6 +147,7 @@ app.patch('/api/workout_plans/:id', async (req,res) => {
     if(obj.plan_name) {
         try {
             const data = await pool.query(`UPDATE workout_plans SET plan_name = '${obj.plan_name}' WHERE plan_id = '${id}';`)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send('Updated plan name');
         } catch (err) {
             console.error(err.message)
@@ -147,6 +156,7 @@ app.patch('/api/workout_plans/:id', async (req,res) => {
     if(obj.type_of_plan) {
         try {
             const data = await pool.query(`UPDATE workout_plans SET type_of_plan = '${obj.type_of_plan}' WHERE plan_id = '${id}';`)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send('Updated type of plan');
         } catch (err) {
             console.error(err.message)
@@ -155,6 +165,7 @@ app.patch('/api/workout_plans/:id', async (req,res) => {
     if(obj.length_of_plan) {
         try {
             const data = await pool.query(`UPDATE workout_plans SET length_of_plan = '${obj.length_of_plan}' WHERE plan_id = '${id}';`)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send('Updated length of plan');
         } catch (err) {
             console.error(err.message)
@@ -166,6 +177,7 @@ app.delete('/api/workout_plans/:id', async (req,res) => {
     const id = req.params.id;
     try {
         const data = await pool.query(`DELETE FROM workout_plans WHERE plan_id = '${id}';`)
+        res.set('Access-Control-Allow-Origin', '*')
         res.send('Deleted Workout Plan')
     } catch (err) {
         console.error(err.message)
@@ -197,6 +209,7 @@ app.post('/api/workout', async (req,res) => {
     const obj = req.body;
     try {
         const data = await pool.query(`INSERT INTO workout (exercise_name, sets, reps_time, rest_cycle) VALUES ('${obj.exercise_name}','${obj.sets}', '${obj.reps_time}', '${obj.rest_cycle}');`)
+        res.set('Access-Control-Allow-Origin', '*')
         res.send('Created User')
     } catch (err) {
         console.error(err.message)
@@ -210,6 +223,7 @@ app.patch('/api/workout/:id', async (req,res) => {
     if(obj.exercise_name) {
         try {
             const data = await pool.query(`UPDATE workout SET exercise_name = '${obj.exercise_name}' WHERE workout_id = '${id}';`)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send('Updated exercise name');
         } catch (err) {
             console.error(err.message)
@@ -218,6 +232,7 @@ app.patch('/api/workout/:id', async (req,res) => {
     if(obj.sets) {
         try {
             const data = await pool.query(`UPDATE workout SET sets = '${obj.sets}' WHERE workout_id = '${id}';`)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send('Updated sets');
         } catch (err) {
             console.error(err.message)
@@ -226,6 +241,7 @@ app.patch('/api/workout/:id', async (req,res) => {
     if(obj.reps) {
         try {
             const data = await pool.query(`UPDATE workout SET reps = '${obj.reps}' WHERE workout_id = '${id}';`)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send('Updated reps');
         } catch (err) {
             console.error(err.message)
@@ -234,6 +250,7 @@ app.patch('/api/workout/:id', async (req,res) => {
     if(obj.reps_time) {
         try {
             const data = await pool.query(`UPDATE workout SET reps_time = '${obj.reps_time}' WHERE workout_id = '${id}';`)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send('Updated reps/time');
         } catch (err) {
             console.error(err.message)
@@ -242,6 +259,7 @@ app.patch('/api/workout/:id', async (req,res) => {
     if(obj.rest_cycle) {
         try {
             const data = await pool.query(`UPDATE workout SET rest_cycle = '${obj.rest_cycle}' WHERE workout_id = '${id}';`)
+            res.set('Access-Control-Allow-Origin', '*')
             res.send('Updated rest cycle');
         } catch (err) {
             console.error(err.message)
@@ -253,6 +271,7 @@ app.delete('/api/workout/:id', async (req,res) => {
     const id = req.params.id;
     try {
         const data = await pool.query(`DELETE FROM workout WHERE workout_id = '${id}';`)
+        res.set('Access-Control-Allow-Origin', '*')
         res.send('Deleted workout')
     } catch (err) {
         console.error(err.message)
