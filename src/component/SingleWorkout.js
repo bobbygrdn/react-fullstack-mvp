21a108fabb40c90c20b4c26a7c9baeed855eb895
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-21 08:12:04 -0700

    Added delete button component

--- src/component/SingleWorkout.js
+++ src/component/SingleWorkout.js
@@ -1,8 +1,11 @@
+import DeleteWorkoutButton from "./DeleteWorkoutButton"
+
 const SingleWorkout = (props) => {
 
     return (
         <div className="singleWorkout">
             <h2 className="singleWorkoutInfo">{props.elem.exercise_name}: {props.elem.sets} X {props.elem.reps_time} Rest Cycle: {props.elem.rest_cycle}</h2>
+            <DeleteWorkoutButton props={props.elem.workout_id} />
         </div>
     )
 }
