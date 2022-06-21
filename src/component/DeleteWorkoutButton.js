f0fd0a1f6ad86850ebb9f10414a6a4cb240d133d
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-21 08:07:09 -0700

    Created delete button for workout items

---
+++ src/component/DeleteWorkoutButton.js
@@ -0,0 +1,26 @@
+const DeleteWorkoutButton = (props) => {
+   
+    let data = {
+        workout_id: `${props.props}`
+    }
+    
+    let fetchData = {
+        method: 'DELETE',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json; charset=UTF-8',
+        })
+    }
+    const handleClick = () => {
+        fetch(`http://localhost:4000/api/workout/${data.workout_id}`, fetchData)
+        .then(() => {
+            window.alert('Workout Dropped')
+        })
+    }
+
+    return (
+        <button className="deleteButton" onClick={handleClick}>Delete</button>
+    )
+}
+
+export default DeleteWorkoutButton
