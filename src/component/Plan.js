27a4518db43913c44faeb262edbc711439879c54
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-20 21:26:52 -0700

    Updated plan component with button

--- src/component/Plan.js
+++ src/component/Plan.js
@@ -1,5 +1,6 @@
-// import spartan from './img/spartan.jpg'
+
 const Plan = (props) => {
+
     return (
         <div className="plan">
             <h1 className="userInfo">Current Plan Info: </h1>
@@ -7,9 +8,11 @@ const Plan = (props) => {
                 <h2 className="planStatistics">Name: {props.plans.data[0].plan_name}</h2>
                 <h2 className="planStatistics">Type: {props.plans.data[0].type_of_plan}</h2>
                 <h2 className="planStatistics">Frequency: {props.plans.data[0].length_of_plan}</h2>
-            </div>
+                <button className='button' >Update</button>
+                </div>
         </div>
     )
+
 }
 
 export default Plan
