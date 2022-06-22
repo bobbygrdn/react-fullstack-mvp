f6400fa74f3ef97a5b01f867addf302ba8585be8
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-21 20:59:15 -0700

    Bug in form makes post request not work

--- src/component/WorkoutForm.js
+++ src/component/WorkoutForm.js
@@ -1,25 +1,52 @@
 import { useState } from "react"
 
-const WorkoutForm = (props) => {
+const WorkoutForm = () => {
 
-    const [name, setName] = useState();
-    const [sets, setSets] = useState();
-    const [reps, setReps] = useState();
-    const [rest, setRest] = useState();
+    const [name, setName] = useState([])
+    const [sets, setSets] = useState([]);
+    const [reps, setReps] = useState([]);
+    const [rest, setRest] = useState([]);
+
+    let data = {
+        exercise_name: name,
+        sets: parseInt(sets),
+        reps_time: reps,
+        rest_cycle: rest
+    }
+
+    let fetchData = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        Headers: new Headers({
+            'Content-Type': 'application/json',
+        })
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        
+        fetch("http://localhost:4000/api/workout", fetchData)
+            .then(() => {
+            window.alert("Workout Added")
+            })
+            .catch(err => {
+                console.error(err);
+            })
+       setName([]);
+       setSets([]);
+       setReps([]);
+       setRest([]);
     }
 
     return (
 
-        <form onSubmit={e => { handleSubmit(e) }}>
+        <form onSubmit={handleSubmit}>
 
-        <input name='name' type='text' value={name} placeholder="Name" onChange={e => setName(e.target.value)} />
-        <input name='type' type='text' value={sets} placeholder="Type" onChange={e => setSets(e.target.value)} />
-        <input name='freq' type='text' value={reps} placeholder="Frequency" onChange={e => setReps(e.target.value)} />
-        <input name='freq' type='text' value={rest} placeholder="Frequency" onChange={e => setRest(e.target.value)} />
-        <input className="button" type='submit' value='Update Plan' />
+            <input name='name' type='text' value={name} placeholder="Name" onChange={(e) => setName(e.target.value)} />
+            <input name='sets' type='text' value={sets} placeholder="Sets" onChange={(e) => setSets(e.target.value)} />
+            <input name='reps' type='text' value={reps} placeholder="Reps" onChange={(e)=> setReps(e.target.value)} />
+            <input name='rest' type='text' value={rest} placeholder="Rest Cycle" onChange={(e) => setRest(e.target.value)} />
+            <input className="button" type='submit' value='Submit' />
       
     </form>
     )
