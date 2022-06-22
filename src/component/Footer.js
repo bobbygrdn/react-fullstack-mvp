01cefe6c8a0d5c81b32b12082015bac84069acd3
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-22 07:51:12 -0700

    created footer component

---
+++ src/component/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => [
+
+    <>
+    <h1 className="footer">“Learn as if you will live forever, live like you will die tomorrow.”  --Mahatma Gandhi</h1>
+    </>
+
+]
+
+export default Footer
