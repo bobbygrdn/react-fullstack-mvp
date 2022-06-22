1aa79097733da6b01fd00efccb8b7d90e65fbba9
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-22 07:35:30 -0700

    Updated component

--- src/component/UserProfile.js
+++ src/component/UserProfile.js
@@ -10,12 +10,9 @@ const UserProfile = (props) => {
     return(
     <div className="profile">
         <h1 className="userData">{props.users.data[0].username}</h1>
-        <h1 className="userInfo">Statistics: </h1>
-        <h2 className="userStats">Height: {props.users.data[0].height}</h2>
-        <h2 className="userStats"> Weight: {props.users.data[0].weight}</h2>
-        <h2 className="userStats"> Body Fat %: {props.users.data[0].body_fat} </h2>
-        <h2 className="userStats"> BMI: {props.users.data[0].bmi}</h2>
-        <>
+        <h1 className="userInfo">Statistics: 
+        </h1>
+        <div>
         {showProfile &&
                 
                 <UserProfileForm showUpdate={setUpdateButton} showProfile={setShowProfile} user={props.users.data[0]} />
@@ -26,6 +23,12 @@ const UserProfile = (props) => {
                 <UserProfileButton showUpdate={setUpdateButton} showProfile={setShowProfile} />
                 
             }
+        </div>
+        <>
+        <h2 className="userStats">Height: {props.users.data[0].height}</h2>
+        <h2 className="userStats"> Weight: {props.users.data[0].weight}</h2>
+        <h2 className="userStats"> Body Fat %: {props.users.data[0].body_fat} </h2>
+        <h2 className="userStats"> BMI: {props.users.data[0].bmi}</h2>
         </>
     </div>
     )
