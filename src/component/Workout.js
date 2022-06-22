b0cb36a8e3eb17f006a5811da8033b2eed2217ff
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-21 23:02:53 -0700

    Added state and functioinality to show and hide components

--- src/component/Workout.js
+++ src/component/Workout.js
@@ -1,14 +1,27 @@
 import SingleWorkout from './SingleWorkout'
 import AddWorkoutButton from './AddWorkoutButton'
 import WorkoutForm from './WorkoutForm'
+import { useState } from 'react'
 
 const Workouts = (props) => {
+  
+  const [workout, showWorkout] = useState(false);
+  const [button, addButton] = useState(true);
 
   return (
       <div className='workouts'>
         <h1 className='workoutsInfo'>Exercises: </h1>
-        <AddWorkoutButton />
-        <WorkoutForm />
+        {workout &&
+                
+                <WorkoutForm showWorkout={showWorkout} addButton={addButton} />
+                
+        }
+        {button &&
+                
+                <AddWorkoutButton showWorkout={showWorkout} addButton={addButton} />  
+                
+        }
+        
           {props.workouts.data.map((elem) => {
             return (        
             <SingleWorkout elem={elem} key={elem.workout_id} />
