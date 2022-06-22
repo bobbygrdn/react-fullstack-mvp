824f3fa9439410d2d89368eb7f0537163a9c6f3b
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-21 21:28:10 -0700

    Completed plan form to update plan details

--- src/component/PlanForm.js
+++ src/component/PlanForm.js
@@ -2,13 +2,33 @@ import { useState } from 'react';
 
 const PlanForm = (props) => {
 
-    const [name, setName] = useState();
-    const [type, setType] = useState();
-    const [freq, setFreq] = useState();
+    const [name, setName] = useState('');
+    const [type, setType] = useState('');
+    const [freq, setFreq] = useState('');
+
+    let data = {
+        plan_name: name,
+        type_of_plan: type,
+        length_of_plan: freq
+    }
+
+    let fetchData = {
+        method: 'PATCH',
+        body: JSON.stringify(data),
+        headers: new Headers ({
+            'Content-Type': 'application/json',
+        })
+    }
 
     const handleSubmit = (e) => {
-        props.updatePlan([name, type, freq])
         e.preventDefault();
+        fetch(`http://localhost:4000/api/workout_plans/${props.plan.plan_id}`, fetchData)
+        .then(() => {
+            window.alert('Changed Workout Plan!')
+        })
+        setName('');
+        setType('');
+        setFreq('');
     }
 
     return(
@@ -18,7 +38,7 @@ const PlanForm = (props) => {
             <input name='name' type='text' value={name} placeholder="Name" onChange={e => setName(e.target.value)} />
             <input name='type' type='text' value={type} placeholder="Type" onChange={e => setType(e.target.value)} />
             <input name='freq' type='text' value={freq} placeholder="Frequency" onChange={e => setFreq(e.target.value)} />
-            <input className="button" type='submit' value='Update Plan' />
+            <input className="button" type='submit' value='Submit' />
           
         </form>
     )
