7e87619afb20cbab767846a5e38e4b059f29dccf
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-21 21:00:07 -0700

    Updated to handle workout form

--- src/component/Workout.js
+++ src/component/Workout.js
@@ -1,9 +1,14 @@
 import SingleWorkout from './SingleWorkout'
+import AddWorkoutButton from './AddWorkoutButton'
+import WorkoutForm from './WorkoutForm'
+
 const Workouts = (props) => {
 
   return (
       <div className='workouts'>
         <h1 className='workoutsInfo'>Exercises: </h1>
+        <AddWorkoutButton />
+        <WorkoutForm />
           {props.workouts.data.map((elem) => {
             return (        
             <SingleWorkout elem={elem} key={elem.workout_id} />
