114162c0898c03f8c07bd4c0a65a035618ca627c
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-06-22 07:35:57 -0700

    updated single workout to implement update button

--- src/component/SingleWorkout.js
+++ src/component/SingleWorkout.js
@@ -1,11 +1,27 @@
+import { useState } from "react"
 import DeleteWorkoutButton from "./DeleteWorkoutButton"
+import UpdateWorkoutForm from './UpdateWorkoutForm'
+import UpdateWorkoutButton from "./UpdateWorkoutButton"
 
 const SingleWorkout = (props) => {
 
+    const [update, showUpdate] = useState(false);
+    const [button, updateButton] = useState(true);
+    
     return (
         <div className="singleWorkout">
             <h2 className="singleWorkoutInfo">{props.elem.exercise_name}: {props.elem.sets} X {props.elem.reps_time} Rest Cycle: {props.elem.rest_cycle}</h2>
             <DeleteWorkoutButton props={props.elem.workout_id} />
+            {update &&
+                
+                <UpdateWorkoutForm showUpdate={showUpdate} UpdateButton={updateButton} workout={props.elem.workout_id} />
+                
+            }
+            {button &&
+                
+                <UpdateWorkoutButton showUpdate={showUpdate} UpdateButton={updateButton} />
+                
+            }
         </div>
     )
 }
